890d33a12cd3ed3143f431b5fcb55ce5b545ba60
Spencer Bloom <spebl@microsoft.com>
2021-06-04 17:29:18 -0700

    fix: update for dev17 (#133)
    Co-authored-by: Cody Miller <codym@microsoft.com>

--- BoostTestPackage/Properties/AssemblyInfo.cs
+++ BoostTestPackage/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
 using System;
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
